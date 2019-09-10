3064cea4e3849881fe4cbc98773833bd7b72b243
GB-Tocix <41319615+GB-Tocix@users.noreply.github.com>
2019-09-11 02:35:30 +0800

    Create Poundage.py

---
+++ 花旗API/Poundage.py
@@ -0,0 +1,19 @@
+def poundage(DealAmount, BuyIn=True, BCRate=0.003):
+    
+    # 用户仅在卖出股票时要缴纳印花税
+    if BuyIn:
+        StampDuty = 0
+    else:
+        StampDuty = DealAmount * 0.001
+
+    # 券商交易佣金最高不超过成交金额的0.3%，单笔交易佣金不满5元按5元收取
+    BrokerageCommission = max(DealAmount * min(0.003, BCRate), 5)
+
+    # 过户费
+    TransferFee = DealAmount * 0.002
+
+    # 总股票交易手续费
+    Poundage = StampDuty + BrokerageCommission + TransferFee
+
+    # 返回结果
+    return Poundage
